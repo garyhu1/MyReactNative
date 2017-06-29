c96bf764d2cd430fa8cd8c6521dd536ff1e58bd4
garyhu <hww9081020@163.com>
2017-06-29 17:57:28 +0800

    新添RN项目

---
+++ AnimateDemo/index.android.js
@@ -0,0 +1,262 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native';
+
+export default class AnimateDemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FadeOut>
+           <Image source = {{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+                style = {{width: 400,height: 400}}/>
+        </FadeOut>
+        <FadeIn>
+            <Text style={{color: '#fff',fontSize: 18}}>悄悄的,我来了</Text>
+        </FadeIn>
+      </View>
+    );
+  }
+}
+
+/*图片慢慢消失，通过控制透明度*/
+class FadeOut extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fadeOutOpacity : new Animated.Value(0),
+		};
+	}
+	
+	render() {
+		return (
+			<Animated.View
+			    style = {{opacity: this.state.fadeOutOpacity,}}>
+			   {this.props.children}
+			</Animated.View>
+		);
+	}
+	
+	startAnim(){
+		this.state.fadeOutOpacity.setValue(1)
+		Animated.timing(
+			this.state.fadeOutOpacity,
+			{
+				toValue: 0,
+				duration: 2000,
+				easing: Easing.linear
+			}
+		).start();
+	}
+	
+	componentDidMount() {
+		this.startAnim();
+	}
+}
+
+/*慢慢的出现*/
+class FadeIn extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			fadeInOpacity: new Animated.Value(0),
+		};
+	}
+	
+	_startAnimation() {
+		Animated.timing(
+			this.state.fadeInOpacity,
+			{
+				toValue: 1,
+				duration: 2000,
+				easing: Easing.linear
+			}
+		).start();
+	}
+	
+	//生命周期函数 render完成后调用
+	componentDidMount() {
+		this._startAnimation();
+	}
+	
+	render() {
+		return (
+			<Animated.View style={{backgroundColor: '#0D8BAD',
+			     opacity: this.state.fadeInOpacity}}>
+			    {this.props.children}
+			</Animated.View>
+		);
+	}
+	
+}
+
+/*混合动画*/
+class MixAnimate extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			fadeInOpacity: new Animated.Value(0),
+			rotation: new Animated.Value(0),
+			fontSize: new Animated.Value(0)
+		}
+	}
+	
+//	getInitialState() {
+//		return ({
+//			fadeInOpacity: new Animated.Value(0),
+//			rotation: new Animated.Value(0),
+//			fontSize: new Animated.Value(0)
+//		});
+//	}
+	
+	componentDidMount() {
+		let timing = Animated.timing;
+		Animated.parallel(['fadeInOpacity','rotation','fontSize'].map(poperty => {
+			return timing(
+				this.state[poperty],
+				{
+					toValue: 1,
+					duration: 2000,
+					easing: Easing.linear
+				}
+			);
+		})).start();
+	}
+	
+	render() {
+		return (
+			<Animated.View style={[styles.animate,{
+				opacity: this.state.fadeInOpacity,
+				transform: [{
+                    rotateZ: this.state.rotation.interpolate({
+                        inputRange: [0,1],
+                        outputRange: ['0deg', '360deg']
+                    })
+                }]
+			}]}>
+			   <Animated.Text style={{
+			   	  fontSize: this.state.fontSize.interpolate({
+                      inputRange: [0,1],
+                      outputRange: [12,26]
+                  })
+			   }}>
+			        我骑着七彩祥云出现了😈💨
+			   </Animated.Text>
+			</Animated.View>
+		);
+	}
+}
+
+/*组合动画*/
+class CombinationAnim extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			anim: [1,2,3].map(() => new Animated.Value(0)) // 初始化3个值
+		}
+	}
+	
+	componentDidMount() {
+		var timing = Animated.timing;
+		Animated.sequence([
+			Animated.stagger(200,this.state.anim.map(left => {
+				return timing(left,{
+					toValue: 1
+				});
+			}).concat(this.state.anim.map(left => {
+				return timing(left,{
+					toValue: 0
+				});
+			}))),// 三个view滚到右边再还原，每个动作间隔200ms
+			Animated.delay(400),// 延迟400ms，配合sequence使用
+			timing(this.state.anim[0],{
+				toValue: 1
+			}),
+			timing(this.state.anim[1],{
+				toValue: -1
+			}),
+			timing(this.state.anim[2],{
+				toValue: 0.5
+			}),
+			Animated.delay(400),
+			Animated.parallel(this.state.anim.map((anim) => {
+				return timing(anim,{
+					toValue: 0
+				});
+			}))
+		]).start();
+	}
+	
+	render() {
+       var views = this.state.anim.map(function(value, i) {
+           return (
+              <Animated.View
+                 key={i}
+                 style={[styles.animate, styles['demo' + i], {
+                    left: value.interpolate({
+                        inputRange: [0,1],
+                        outputRange: [0,200]
+                    })
+              }]}>
+                <Text style={styles.text}>我是第{i + 1}个View</Text>
+ 
+              </Animated.View>
+            );
+        });
+      return <View style={styles.container}>
+               <Text>sequence/delay/stagger/parallel演示</Text>
+               {views}
+             </View>;
+}
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  animate: {
+  	flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  demo0:{
+  	backgroundColor: 'red'
+  },
+  demo2:{
+  	backgroundColor: 'darkgreen'
+  },
+  demo1: {
+  	backgroundColor: 'skyblue'
+  },
+  text: {
+  	fontSize: 15
+  }
+});
+
+AppRegistry.registerComponent('AnimateDemo', () => CombinationAnim);
--- NavigationDemo/index.android.js
+++ NavigationDemo/index.android.js
@@ -4,18 +4,40 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	TouchableWithoutFeedback,
+	Animated,
 } from 'react-native';
 
+import { StackNavigator } from 'react-navigation';
+
 export default class NavigationDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
+	constructor(props) {
+		super(props);
+		this.state = {
+			showText: false
+		};
+	}
+
+	_btnClick() {
+		//	this.setState({show:!this.state.showText});
+		//	console.log('onPress');
+	}
+
+	render() {
+		let display = this.state.showText ? "garyhu" : "";
+		return(
+			<View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -26,28 +48,167 @@ export default class NavigationDemo extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+        <TouchableOpacity onPress={this._btnClick} style={styles.btn}>
+           <Text style={styles.btnText}>Button</Text>
+        </TouchableOpacity>
+        <TouchableHighlight onPress={this._btnClick} style={styles.touchBtn}>
+            <Text>Button</Text>
+        </TouchableHighlight>
+        <TouchableNativeFeedback
+            onPress={this._onPressButton}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+            <View style={styles.touchBtn}>
+               <Text>Button</Text>
+            </View>
+        </TouchableNativeFeedback>
+        <TouchableWithoutFeedback onPress={this._btnClick} style={styles.touchBtn}>
+            <View style={styles.touchBtn}>
+                 <Text>Button</Text>
+            </View>
+        </TouchableWithoutFeedback>
+        <Text>{display}</Text>
       </View>
-    );
-  }
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+/*动画组件*/
+class FadeInView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fadeAnim: new Animated.Value(0), //透明度初始值设为0
+		};
+	}
+
+	componentDidMount() {
+		Animated.timing(
+			this.state.fadeAnim, {
+				toValue: 1,
+				duration: 2000,
+			}
+		).start();
+	}
+
+	render() {
+		return(
+			<Animated.View style={{width:300,height:60,backgroundColor: "#0F6C83",opacity: this.state.fadeAnim}}>
+			   {this.props.children}
+			</Animated.View>
+		);
+	}
+}
+
+/*组合动画*/
+class CombinationAnim extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	Animated.sequence([ // 首先执行decay动画，结束后同时执行spring和twirl动画
+		Animated.decay(position, { // 滑行一段距离后停止
+			velocity: {
+				x: gestureState.vx,
+				y: gestureState.vy
+			}, // 根据用户的手势设置速度
+			deceleration: 0.997,
+		}),
+		Animated.parallel([ // 在decay之后并行执行：
+			Animated.spring(position, {
+				toValue: {
+					x: 0,
+					y: 0
+				} // 返回到起始点开始
+			}),
+			Animated.timing(twirl, { // 同时开始旋转
+				toValue: 360,
+			}),
+		]),
+	]).start(); // 执行这一整套动画序列
+	
+	render() {
+		return (
+			<Animated.View>
+			   {this.props.children}
+			</Animated.View>
+		);
+	}
+}
+
+class MyAnimate extends Component {
+	render() {
+		return(
+			<View style={styles.container}>
+			   <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+			       <Text style={{fontSize: 28, textAlign: 'center', margin: 10,color: '#fff'}}>Fading in</Text>
+			   </FadeInView>
+			   <CombinationAnim>
+			        <Text style={{fontSize: 28, textAlign: 'center', margin: 10,color: '#fff'}}>Sequence Anim</Text>
+			   <CombinationAnim>
+			</View>
+		);
+	}
+}
+
+class HomeScreen extends Component {
+	static navigationOptions = {
+		title: 'Welcome',
+	};
+	render() {
+		return <Text>Hello, Navigation!</Text>;
+	}
+}
+
+const SimpleApp = StackNavigator({
+	Home: {
+		screen: HomeScreen
+	},
 });
 
-AppRegistry.registerComponent('NavigationDemo', () => NavigationDemo);
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+	btn: {
+		width: 80,
+		height: 40,
+		backgroundColor: '#0F95B9',
+		borderWidth: 1,
+		borderStyle: 'solid',
+		borderColor: '#03313B',
+		borderRadius: 5,
+		marginTop: 10,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	btnText: {
+		color: '#fff',
+		fontSize: 12,
+	},
+	touchBtn: {
+		width: 100,
+		height: 40,
+		marginTop: 10,
+		borderWidth: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderStyle: 'solid',
+		borderColor: '#111111',
+		borderRadius: 5,
+	}
+});
+
+AppRegistry.registerComponent('NavigationDemo', () => MyAnimate);
